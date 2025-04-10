30da10e3cd58ec961712fdec8284fed5c8aa94b5
Christopher Bennell <cbennell@hey.com>
2025-04-10 14:30:08 -0400

    Add new ruby LSP config

---
+++ .config/nvim/after/lsp/ruby_lsp.lua
@@ -0,0 +1,13 @@
+-- return {
+--   cmd = { "ruby-lsp" },
+--   filetypes = { "ruby" },
+--   root_markers = {
+--     "Gemfile.lock",
+--     "Gemfile",
+--     ".ruby-version",
+--   },
+--   init_options = {
+--     formatter = "standard",
+--     linters = { "standard" },
+--   },
+-- }
