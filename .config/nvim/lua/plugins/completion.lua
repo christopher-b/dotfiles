67c47eca71c5e2da7c07f1ee8299628722f84e9d
Christopher Bennell <cbennell@hey.com>
2025-05-02 09:49:16 -0400

    Fix completion config

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -10,27 +10,27 @@ return {
   opts = {
     sources = {
       default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
-    },
-    providers = {
-      ripgrep = {
-        module = "blink-ripgrep",
-        name = "Ripgrep",
-        opts = {
-          max_filesize = "100K",
-          search_casing = "--smart-case",
+      providers = {
+        ripgrep = {
+          module = "blink-ripgrep",
+          name = "Ripgrep",
+          opts = {
+            max_filesize = "100K",
+            search_casing = "--smart-case",
+          },
+          -- (optional) customize how the results are displayed. Many options
+          -- are available - make sure your lua LSP is set up so you get
+          -- autocompletion help
+          transform_items = function(_, items)
+            for _, item in ipairs(items) do
+              -- example: append a description to easily distinguish rg results
+              item.labelDetails = {
+                description = "(rg)",
+              }
+            end
+            return items
+          end,
         },
-        -- (optional) customize how the results are displayed. Many options
-        -- are available - make sure your lua LSP is set up so you get
-        -- autocompletion help
-        transform_items = function(_, items)
-          for _, item in ipairs(items) do
-            -- example: append a description to easily distinguish rg results
-            item.labelDetails = {
-              description = "(rg)",
-            }
-          end
-          return items
-        end,
       },
     },
     keymap = { preset = "super-tab" },
